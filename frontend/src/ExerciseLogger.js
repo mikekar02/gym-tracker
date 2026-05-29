afd8e64ec819afba725aaf3e47011b0b0b256a3f
Mike Karvelis <mikekar02@gmail.com>
2026-05-30 01:43:52 +0300

    started working on the frontend

---
+++ frontend/src/ExerciseLogger.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+function ExerciseLogger({ workoutId, onSetLogged }) {
+    const [exercise, setExercises] = useState([]);
+    const [selectedExercise, setSelectedExercise] = useState("");
+    const [weight, setWeight] = useState("");
+    const [reps, setReps] = useState("");
+}
